82d00d45687f136595f543f8eb680ed01bc1ca38
DilanMalaka <malakanawarathna280@gmail.com>
2021-09-05 13:14:49 +0530

    Initial Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,152 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f1.jpg',
+            price: 120,
+            countInStock:10,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '2',
+            name: 'Chopsey Rice 2',
+            category: 'Rice',
+            image: '/images/f2.jpg',
+            price: 120,
+            countInStock:0,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '3',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f3.jpg',
+            price: 120,
+            countInStock:30,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '4',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f4.jpg',
+            price: 120,
+            countInStock:40,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '5',
+            name: 'Chopsey Rice',
+            category: 'Rice',
+            image: '/images/f5.jpg',
+            price: 120,
+            countInStock:50,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '6',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f6.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '7',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f7.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '8',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f8.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '9',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f9.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '10',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f10.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '11',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f11.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+        {
+            _id: '12',
+            name: 'Chopsey Rice 6',
+            category: 'Rice',
+            image: '/images/f12.jpg',
+            price: 120,
+            countInStock:60,
+            brand: 'BREEZE',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality food'
+        },
+
+
+    ],
+};
+
+export default data;
