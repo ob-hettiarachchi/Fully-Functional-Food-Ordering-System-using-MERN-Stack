f95e91c74dff1ad45ad5de9b1e95227c23825cbb
DilanMalaka <malakanawarathna280@gmail.com>
2021-09-08 13:55:31 +0530

    8/9/2021
    created order screen, added PayPal button, implemented order payment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://adminBreeze:test1234@breezedb.mvy8p.mongodb.net/breeseDB?retryWrites=true&w=majority", 
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://adminBreeze:test1234@breezedb.mvy8p.mongodb.net/foodorderdb?retryWrites=true&w=majority", 
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,6 +23,10 @@ app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 
 app.use('/api/orders', orderRouter);
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 
 app.get('/',(req, res) => {
     res.send('Server is ready');
